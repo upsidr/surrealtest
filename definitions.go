9b54d875a5aab51ea2d2c7b95df0b36aa249b02d
Ryota <rytswd@gmail.com>
2023-07-18 16:52:36 +0100

    Add clarification comment for public variables (#2)

--- definitions.go
+++ definitions.go
@@ -1,6 +1,14 @@
 package surrealtest
 
 var (
+	// SurrealDBRepo is the repository name of SurrealDB image.
+	//
+	// If you would like to use some other version, you can update this variable
+	// to point to the image you want to use.
 	SurrealDBRepo = "surrealdb/surrealdb"
-	SurrealDBTag  = "1.0.0-beta.9-20230402"
+	//SurrealDBTag is the tag of SurrealDB image.
+	//
+	// If you would like to use some other version, you can update this variable
+	// to point to the image you want to use.
+	SurrealDBTag = "1.0.0-beta.9-20230402"
 )
